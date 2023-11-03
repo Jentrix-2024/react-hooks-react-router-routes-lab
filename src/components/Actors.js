99abb93df81ab460ef5204fed88d7c1346726f9a
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-11-03 03:56:16 +0300

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,23 @@
 import React from "react";
+//import actors data from /data
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, movieIndex) => (
+              <li key={movieIndex}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,73 @@
+// src/components/NavBar.js
 import React from "react";
 import { NavLink } from "react-router-dom";
+//style the Navbar
+const linkStyles = {
+  display: "inline-block",
+  width: "100px", 
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+          Actors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        
+        Movies
+      </NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Movies
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
